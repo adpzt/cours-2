3c787158329f8ac4c8cdc6b0bcf52ac3da427396
adpzt <145431685+adpzt@users.noreply.github.com>
2023-09-22 11:48:40 +0200

    Add files via upload

---
+++ cours 2.js
@@ -0,0 +1,13 @@
+"use scrict"
+
+function saluer() {
+    console.log("Bonjour moi c prout et je suis addict a la drogue")
+}
+saluer()
+
+let pseudo = "en vrai je m'appelle adrien ou adpzt999"
+function saluerQqun(prenom){
+    console.log("Bonjour " + prenom)
+}
+saluerQqun ("Prout")
+saluerQqun (pseudo)
